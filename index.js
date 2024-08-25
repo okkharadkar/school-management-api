793cf6b489a0a3b66206faa58743411cba6bf1a6
Om Kharadkar <omkharadkar664@gmail.com>
2024-08-26 01:06:50 +0530

    Configured environment variables for Vercel deployment

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json());
 
 app.use('/', schoolRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 28171;
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
 });
